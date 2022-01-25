aa975a26835b69e9e29fef9813621bdf57baf398
Narisimkha <73960346+Narisimkha@users.noreply.github.com>
2022-01-25 21:45:49 +0200

    Add files via upload
    Подготовлена страница ,переименовано: эстонские названия на английские, Реализация новостей.

---
+++ cats - этап 1/admin/viewAdmin/startAdmin.php
@@ -0,0 +1,12 @@
+<?php ob_start(); ?>
+<h2>Admins Panel </h2>
+<article>
+
+    <p>Admins Panel </p>
+
+
+</article>
+<?php $content = ob_get_clean(); ?>
+
+<?php
+include "viewAdmin/templates/layout.php";
