07828cbb7d50793450c4961037c4f69284c03199
Narisimkha <73960346+Narisimkha@users.noreply.github.com>
2022-01-25 21:54:14 +0200

    Add files via upload
    Замена цвета фона редактирование файла style.css подбор цвета + редактирование текста .Проверка отправки комментария к новости прошла успешно.

---
+++ cats - этап 4/route/routing.php
@@ -0,0 +1,38 @@
+<?php
+
+//вычислить маршрут из адресной строки
+$host = explode('?', $_SERVER['REQUEST_URI'])[0];
+$num = substr_count($host, '/');
+$path = explode('/', $host)[$num];
+
+
+if ($path == '' OR $path == 'index' OR $path == 'index.php') {
+    $response = Controller::StartSite();
+} elseif ($path == 'all') {
+    $response = Controller::AllNews();
+} elseif ($path == 'category' and isset($_GET['id'])) {
+    $response = Controller::NewsByCatID($_GET['id']);
+} elseif ($path == 'news' and isset($_GET['id'])) {
+    $response = Controller::NewsByID($_GET['id']);
+} elseif ($path == 'search' and isset($_GET['search']))
+{    $response = Controller::SearchNews($_GET['search']);
+} elseif ($path == 'insertcomment' and isset($_GET['comment'], $_GET['id'])) {
+    $response = Controller::InsertComment($_GET['comment'], $_GET['id']);
+} elseif ($path == 'info') {
+    $response = Controller::info();
+}
+//--------------register user
+elseif ($path == 'registerForm' )
+{   //from register
+    
+    $response = Controller::registerForm();
+}
+elseif ($path == 'registerAnswer' )
+{   //register user
+    $response = Controller::registerUser();
+}
+//error page
+else {
+    $response = Controller::error404();
+}
+?>
---
+++ cats - этап 4/view/news.php
@@ -0,0 +1,45 @@
+<?php
+
+class ViewNews {
+
+    public static function NewsByCategory($arr) {
+        echo '<div class="container">';
+        echo '<div class="row">';
+        
+        foreach ($arr as $value) {
+            echo '<div class="col">';
+            echo '<img src="data:image/jpeg;base64,' . base64_encode($value['picture']) . '" width=150 /><br>';
+            echo "<h2 >" . $value['title'] . "<h2>";
+            Controller::CommentsCount($value['id']);
+            echo "<a href='news?id=" . $value['id'] . "'>Next</a><br>";
+            echo '</div>';
+        }
+        echo '</div>';
+        echo '</div>';
+        
+        
+    }
+
+    public static function AllNews($arr) {
+        
+        foreach ($arr as $value) {
+            echo "<li >". $value['title'];
+            Controller::CommentsCount($value['id']);
+            echo "<a href='news?id=".$value['id']."'>Next</a></li><br>";
+        }
+       
+        
+    }
+
+    public static function ReadNews($n) {
+        echo "<h2 >".$n['title']. "<h2>";
+        Controller::CommentsCountWithAncor($n['id']);
+        echo '<br><img src="data:image/jpeg;base64,'.base64_encode( $n['picture']) . '" width=150 /><br>';
+        echo "<p>".$n['text']."<p>";
+    }
+    
+
+}//end class
+
+
+
